a6a098e338e5d0d49aca491d58622bf7f84684a4
Caolan <caolanhamilton75@gmail.com>
2022-10-13 10:14:33 +0100

    init repo

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+    app := fiber.New()
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, Snoopy 🐶 & Stumpy 🐈 👋!")
+    })
+
+    app.Listen(":3000")
+}
